c343f17f5e1baaf1dfd9003f8fca0293ed6d8e23
ahnaf889 <dreamlessboyahnaf8@gmail.com>
2024-03-25 12:49:53 +0600

    js text

---
+++ Js/main.js
@@ -0,0 +1,22 @@
+// Ten word home work: sum, one, two, three,  four, five, six, seven, eight, nine, ten
+
+var first = 100;
+var secound = 200;
+var third =  300;
+var fourth = 400;
+var fifth = 500;
+var sixth = 600;
+var seventh = 700;
+var eighth = 800;
+var nineth = 900;
+var tenth = 1000;
+
+var sum = first + secound;
+var subscription = tenth - fourth;
+var multiplication = fifth * sixth;
+var division = seventh / eighth;
+
+console.log("First result :", sum);
+console.log("Secound result :", subscription);
+console.log("Third Result :", multiplication);
+console.log("fourth Result :", division);
